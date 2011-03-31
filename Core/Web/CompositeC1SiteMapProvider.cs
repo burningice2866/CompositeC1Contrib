790ab0612235e35142b34de72b32ced38807f644
burningice <none@none>
2011-03-31 22:24:53 +0000

    - Moved Mvc features into own project for easier compilation  - Added several rendering-controls to match the rendering-markup in C1 xml templates    - <rendering:Title /> and <rendering:Description />  - Changed location of Masterpages from ~/Renderers/Masters to ~/App_Data/PageTemplates  - Changed the way you insert page-content into a master     - use <rendering:placeholder /> to match C1 xml template format  - added possibility to execute and render C1 functions directly on from webforms     - use <rendering:function> C1 markup goes here </rendering:function>  - added an option to web.config/sppSettings to use the url filter but still keep the .aspx extension  - fixed issues with url filter not working in virtual directories

---
+++ Core/Web/CompositeC1SiteMapProvider.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+using Composite.C1Console.Security;
+using Composite.Data;
+using Composite.Data.Types;
+
+namespace CompositeC1Contrib.Web
+{
+    public class CompositeC1SiteMapProvider : BaseSiteMapProvider
+    {
+        private PublicationScope PublicationScope
+        {
+            get
+            {
+                if ((HttpContext.Current.Request.QueryString["dataScope"] == "administrated" && UserValidationFacade.IsLoggedIn())
+                    || RequestInfo.Current.IsPreview)
+                {
+                    return PublicationScope.Unpublished;
+                }
+
+                return PublicationScope.Published;
+            }
+        }
+
+        protected override bool CanCache
+        {
+            get { return PublicationScope == PublicationScope.Published; }
+        }
+
+        protected override string GetCurrentKey()
+        {
+            return SitemapNavigator.CurrentPageId.ToString();
+        }
+
+        public override bool IsAccessibleToUser(HttpContext ctx, SiteMapNode node)
+        {
+            if (PublicationScope == PublicationScope.Unpublished)
+            {
+                return true;
+            }
+
+            if (ExtranetEnabled)
+            {
+                throw new NotImplementedException("Extranet is not implemented in the currect C1 sitemap provider");
+            }
+
+            return true;
+        }
+
+        public override void Initialize(string name, NameValueCollection attributes)
+        {
+            DataEventHandler handler = (sender, e) => Flush();
+
+            DataEventSystemFacade.SubscribeToDataAfterAdd<IPage>(handler);
+            DataEventSystemFacade.SubscribeToDataAfterUpdate<IPage>(handler);
+            DataEventSystemFacade.SubscribeToDataDeleted<IPage>(handler);
+
+            DataEventSystemFacade.SubscribeToDataAfterAdd<IPageStructure>(handler);
+            DataEventSystemFacade.SubscribeToDataAfterUpdate<IPageStructure>(handler);
+            DataEventSystemFacade.SubscribeToDataDeleted<IPageStructure>(handler);
+
+            DataEventSystemFacade.SubscribeToDataAfterAdd<ISystemActiveLocale>(handler);
+            DataEventSystemFacade.SubscribeToDataAfterUpdate<ISystemActiveLocale>(handler);
+            DataEventSystemFacade.SubscribeToDataDeleted<ISystemActiveLocale>(handler);
+
+            base.Initialize(name, attributes);
+        }
+
+        protected override void LoadSiteMapInternal(IDictionary<CultureInfo, SiteMapContainer> list, string host)
+        {
+            var scope = PublicationScope;
+
+            if (scope == PublicationScope.Unpublished)
+            {
+                host = ensureCorrectHost(host);
+            }
+
+            foreach (var ci in DataLocalizationFacade.ActiveLocalizationCultures)
+            {
+                using (var data = new DataConnection(scope, ci))
+                {
+                    var root = data.SitemapNavigator.GetPageNodeByHostname(host);
+                    if (root != null)
+                    {
+                        var container = new SiteMapContainer()
+                        {
+                            Root = new CompositeC1SiteMapNode(this, root, data)
+                        };
+
+                        list.Add(ci, container);
+
+                        loadNodes(root, null, container, data);
+                    }
+                }
+            }
+        }
+
+        protected override void AddRolesInternal(IDictionary<CultureInfo, SiteMapContainer> list) { }
+
+        private string ensureCorrectHost(string host)
+        {
+            using (var data = new DataConnection())
+            {
+                var websites = data.SitemapNavigator.HomePageIds;
+                if (websites.Count() > 1)
+                {
+                    IPage page = null;
+
+                    var currentNode = data.SitemapNavigator.CurrentHomePageNode;
+                    if (currentNode != null)
+                    {
+                        page = data.Get<IPage>().SingleOrDefault(p => p.Id == currentNode.Id);
+                    }
+                    
+                    if (page == null)
+                    {
+                        var path = HttpContext.Current.Request.Url.LocalPath;
+                        int secondSlash;
+
+                        if (data.Get<ISystemActiveLocale>().Count() > 1)
+                        {
+                            secondSlash = path.IndexOf("/", 1);
+                            path = path.Remove(0, secondSlash == -1 ? path.Length : secondSlash);
+                        }
+
+                        if (path.Length > 0)
+                        {
+                            secondSlash = path.IndexOf("/", 1);
+                            var currentWebsite = path.Substring(1, (secondSlash == -1 ? path.Length : secondSlash) - 1).Replace(".aspx", String.Empty);
+
+                            page = websites
+                                .Select(site => data.Get<IPage>().Single(p => p.Id == site))
+                                .SingleOrDefault(p => p.UrlTitle.Equals(currentWebsite, StringComparison.OrdinalIgnoreCase));
+                        }
+                    }
+
+                    if (page != null)
+                    {
+                        var hostNameBinding = data.Get<IPageHostNameBinding>().SingleOrDefault(p => p.PageId == page.Id);
+                        if (hostNameBinding != null)
+                        {
+                            return hostNameBinding.HostName;
+                        }
+                    }
+                }
+            }
+
+            return host;
+        }
+
+        private void loadNodes(PageNode pageNode, SiteMapNode parent, SiteMapContainer container, DataConnection data)
+        {
+            var node = new CompositeC1SiteMapNode(this, pageNode, data);
+            AddNode(node, parent, container);
+
+            var childs = pageNode.ChildPages;
+            foreach (var child in childs)
+            {
+                loadNodes(child, node, container, data);
+            }
+        }
+
+        public static CompositeC1SiteMapNode ResolveNodeFromUrl(Uri url)
+        {
+            string path = url.LocalPath;
+            var provider = (CompositeC1SiteMapProvider)SiteMap.Provider;
+
+            var ci = DataLocalizationFacade.ActiveLocalizationCultures.SingleOrDefault(c => path.StartsWith("/" + c.TwoLetterISOLanguageName, StringComparison.OrdinalIgnoreCase));
+            if (ci == null)
+            {
+                ci = DataLocalizationFacade.DefaultLocalizationCulture;
+            }
+
+            if (url.Query.Contains("dataScope=administrated"))
+            {
+                path += "?dataScope=administrated";
+            }
+
+            var node = provider.FindSiteMapNode(path, ci) as CompositeC1SiteMapNode;
+            if (node == null)
+            {
+                if (UrlUtils.IsDefaultDocumentUrl(path))
+                {
+                    node = provider.RootNode as CompositeC1SiteMapNode;
+                }
+            }
+
+            return node;
+        }
+    }
+}
---
+++ Core/Web/UrlFilterModule.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Web;
+
+using CompositeC1Contrib.Web.UI;
+
+namespace CompositeC1Contrib.Web
+{
+    public class UrlFilterModule : IHttpModule
+    {
+        void IHttpModule.Dispose() { }
+
+        void IHttpModule.Init(HttpApplication app)
+        {
+            app.BeginRequest += new EventHandler(app_BeginRequest);
+            app.PostRequestHandlerExecute += new EventHandler(app_PostRequestHandlerExecute);
+        }
+
+        protected virtual void RewritePath(HttpContext ctx)
+        {
+            string path = ctx.Request.Url.LocalPath;
+            string extension = Path.GetExtension(path);
+
+            if (UrlUtils.IsDefaultDocumentUrl(path) || String.IsNullOrEmpty(extension) || String.Equals(extension, ".aspx", StringComparison.OrdinalIgnoreCase))
+            {
+                var node = CompositeC1SiteMapProvider.ResolveNodeFromUrl(ctx.Request.Url);
+
+                if (node != null)
+                {
+                    string query = ctx.Request.Url.Query;
+                    if (!String.IsNullOrEmpty(query))
+                    {
+                        query = query.Remove(0, 1);
+                    }
+
+                    ctx.RewritePath(node.PageNode.Url, ctx.Request.PathInfo, query);
+                }
+            }
+        }
+
+        protected virtual bool ShouldRewriteUrls(HttpContext ctx)
+        {
+            return ctx.Handler is CompositeC1Page;       
+        }
+
+        private void app_BeginRequest(object sender, EventArgs e)
+        {
+            var ctx = ((HttpApplication)sender).Context;
+            RewritePath(ctx);
+        }
+
+        private void app_PostRequestHandlerExecute(object sender, EventArgs e)
+        {
+            var ctx = ((HttpApplication)sender).Context;
+
+            if (ctx.Response.StatusCode != (int)HttpStatusCode.InternalServerError
+                && ShouldRewriteUrls(ctx))
+            {
+
+                ctx.Response.Filter = new UrlFilter(ctx.Response.Filter, ctx);
+            }
+        }
+    }
+}
