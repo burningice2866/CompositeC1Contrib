52446fb485556ae3a4e6ee9aa4dd711d0459464a
Pauli Østerø <poe@acto.dk>
2013-04-23 17:32:02 +0200

    Added base UserControl class which can be inherited to get access to default C1-related data and functionality.

---
+++ UserControlFunctions/Web/UI/CompositeC1UserControl.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Web.UI;
+
+using Composite.Data;
+
+namespace CompositeC1Contrib.UserControlFunctions.Web.UI
+{
+    public class CompositeC1UserControl : UserControl
+    {
+        private bool _disposed = false;
+        private DataConnection _data;
+
+        public DataConnection Data
+        {
+            get
+            {
+                if (_data == null)
+                {
+                    _data = new DataConnection();
+                }
+
+                return _data;
+            }
+        }
+
+        public SitemapNavigator Sitemap
+        {
+            get { return Data.SitemapNavigator; }
+        }
+
+        public PageNode HomePageNode
+        {
+            get { return Sitemap.CurrentHomePageNode; }
+        }
+
+        public PageNode CurrentPageNode
+        {
+            get { return Sitemap.CurrentPageNode; }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _data.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        ~CompositeC1UserControl()
+        {
+            Dispose(false);
+        }
+    }
+}
