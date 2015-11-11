40649b825a85f6c73296119fe081ae4d95a7394e
Pauli Østerø <poe@cphcloud.com>
2015-11-11 18:41:19 +0100

    Ability to use the IJobCancellationToken interface as an argument for the Scheduled Task method.

--- ScheduledTasks/Configuration/ScheduledTasksSection.cs
+++ ScheduledTasks/Configuration/ScheduledTasksSection.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 
 using Hangfire;
 
@@ -36,16 +39,51 @@ namespace CompositeC1Contrib.ScheduledTasks.Configuration
                     throw new ArgumentException(String.Format("Type '{0}' doesn't exist", element.Type));
                 }
 
-                var method = type.GetMethod(element.Method);
-                if (method == null)
+                var methods = type.GetMethods().Where(m => m.Name == element.Method).ToList();
+                if (!methods.Any())
                 {
                     throw new ArgumentException(String.Format("Method '{0}' doesn't exist on type {1}", element.Method, type.FullName));
                 }
 
-                var action = Expression.Lambda<Action>(Expression.Call(method));
+                var methodToCall = FindMethodToCall(methods);
+                if (methodToCall == null)
+                {
+                    throw new ArgumentException(String.Format("Method '{0}' doesn't have a valid signature. It needs to be parameterless or take a 'IJobCancellationToken'", element.Method));
+                }
+
+                Expression<Action> action;
+                if (methodToCall.GetParameters().Any())
+                {
+                    var argument = Expression.Constant(JobCancellationToken.Null, typeof(IJobCancellationToken));
+
+                    action = Expression.Lambda<Action>(Expression.Call(methodToCall, argument));
+                }
+                else
+                {
+                    action = Expression.Lambda<Action>(Expression.Call(methodToCall));
+                }
 
                 RecurringJob.AddOrUpdate(element.Name, action, element.CronExpression);
             }
+        }
+
+        private static MethodInfo FindMethodToCall(IEnumerable<MethodInfo> methods)
+        {
+            foreach (var method in methods)
+            {
+                var parameters = method.GetParameters();
+                if (parameters.Length == 1 && parameters[0].ParameterType == typeof (IJobCancellationToken))
+                {
+                    return method;
+                }
+
+                if (parameters.Length == 0)
+                {
+                    return method;
+                }
+            }
+
+            return null;
         }
     }
 }
