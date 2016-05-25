388d086616796d9bda1c4925f38a4259d20ee628
Pauli Østerø <pauli@burningice.dk>
2016-05-25 16:45:36 +0200

    Initial commit of a new Stackify package which includes a TraceListener for sending C1 logs to Stackify

---
+++ Stackify/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CompositeC1Contrib.Stackify")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BurningIce")]
+[assembly: AssemblyProduct("CompositeC1Contrib.Stackify")]
+[assembly: AssemblyCopyright("Copyright © BurningIce 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7044a5f4-2dda-4cf6-a130-a2ff09bb97b3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Stackify/StackifyTraceListener.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+using Microsoft.Practices.EnterpriseLibrary.Logging.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Logging.TraceListeners;
+
+using Composite.C1Console.Events;
+using Composite.Core.Configuration;
+
+using StackifyLib;
+using StackifyLib.Internal.Metrics;
+using StackifyLib.Models;
+using StackifyLib.Utils;
+
+using StackifyLogger = StackifyLib.Logger;
+
+namespace CompositeC1Contrib.Stackify
+{
+    [ConfigurationElementType(typeof(CustomTraceListenerData))]
+    public class StackifyTraceListener : CustomTraceListener
+    {
+        public StackifyTraceListener()
+        {
+            StackifyAPILogger.Log("Composite C1 TraceListener created");
+        }
+
+        static StackifyTraceListener()
+        {
+            GlobalEventSystemFacade.SubscribeToPrepareForShutDownEvent(args =>
+            {
+                StackifyAPILogger.Log("Composite C1 TraceListener shutting down");
+                MetricClient.StopMetricsQueue("Composite C1 TraceListener shutting down");
+
+                StackifyLogger.Shutdown();
+            });
+        }
+
+        public override void TraceData(TraceEventCache eventCache, string source, TraceEventType eventType, int id, object data)
+        {
+            var entry = (LogEntry)((ICloneable)data).Clone();
+
+            entry.Title = SanatizeTitle(entry.Title);
+
+            var stackifyEvent = Translate(eventType, entry);
+
+            StackifyLogger.QueueLogObject(stackifyEvent);
+        }
+
+        private static LogMsg Translate(TraceEventType eventType, LogEntry entry)
+        {
+            var stackifyEvent = new LogMsg
+            {
+                Level = MapLevel(eventType),
+                Msg = entry.Title + ": " + entry.Message,
+                data = HelperFunctions.SerializeDebugData(entry, true),
+
+                AppDetails = new LogMsgGroup
+                {
+                    AppName = GlobalSettingsFacade.ApplicationName,
+                    AppNameID = InstallationInformationFacade.InstallationId,
+                    ServerName = entry.MachineName,
+                },
+
+                Tags = new List<string>
+                {
+                    GlobalSettingsFacade.ApplicationName,
+                    InstallationInformationFacade.InstallationId.ToString()
+                }
+            };
+
+            if (entry.ExtendedProperties.ContainsKey("Exception"))
+            {
+                var exc = entry.ExtendedProperties["Exception"] as Exception;
+                if (exc != null)
+                {
+                    stackifyEvent.Ex = StackifyError.New(exc);
+                    stackifyEvent.Msg = entry.Title + ": " + exc.Message;
+                }
+            }
+
+            return stackifyEvent;
+        }
+
+        private static string SanatizeTitle(string title)
+        {
+            if (!title.Contains(")"))
+            {
+                return title;
+            }
+
+            var index = title.LastIndexOf(")", StringComparison.Ordinal);
+
+            return title.Substring(index + 1).Trim();
+        }
+
+        public override void Write(string message) { }
+        public override void WriteLine(string message) { }
+
+        private static string MapLevel(TraceEventType type)
+        {
+            switch (type)
+            {
+                case TraceEventType.Critical:
+                case TraceEventType.Error: return "ERROR";
+
+                case TraceEventType.Warning: return "WARNING";
+                case TraceEventType.Information: return "INFORMATION";
+                case TraceEventType.Verbose: return "VERBOSE";
+
+                default: return "NOTICE";
+            }
+        }
+    }
+}
