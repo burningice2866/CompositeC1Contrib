f8e9ab51951afab0383d09d91ce6f8d83a6b79b7
Pauli Østerø <pauli@burningice.dk>
2016-06-23 10:05:16 +0200

    Added helpers to CompositeC1Contrib.Core and bumped version to 0.7.0

---
+++ Core/FunctionContextContainerExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+using Composite.Functions;
+
+namespace CompositeC1Contrib
+{
+    public static class FunctionContextContainerExtensions
+    {
+        private static FieldInfo _field = typeof(FunctionContextContainer).GetField("_parameterDictionary", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        public static T GetParameter<T>(this FunctionContextContainer container, string name)
+        {
+            try
+            {
+                return (T)container.GetParameterValue(name, typeof(T));
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+
+        public static bool TryGetParameter<T>(this FunctionContextContainer container, string name, out T value)
+        {
+            try
+            {
+                value = (T)container.GetParameterValue(name, typeof(T));
+
+                return true;
+            }
+            catch
+            {
+                value = default(T);
+
+                return false;
+            }
+        }
+
+        public static void AddParameter(this FunctionContextContainer container, string name, object parameter)
+        {
+            var díctionary = (Dictionary<string, object>)_field.GetValue(container);
+            if (díctionary == null)
+            {
+                díctionary = new Dictionary<string, object>();
+
+                _field.SetValue(container, díctionary);
+            }
+
+            díctionary.Add(name, parameter);
+        }
+    }
+}
---
+++ Core/Web/RouteCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Routing;
+
+namespace CompositeC1Contrib.Web
+{
+    public static class RouteCollectionExtensions
+    {
+        public static void AddGenericHandler<T>(this RouteCollection routes, string route) where T : IHttpHandler, new()
+        {
+            routes.Add(new Route(route, new GenericRouteHandler<T>()));
+        }
+    }
+}
