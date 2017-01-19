8cb31b946cae7f4d93e121141f3341a943556179
Pauli Østerø <pauli@burningice.dk>
2017-01-19 11:06:33 -0300

    Updated NewRelic package

---
+++ NewRelic/AppStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+
+using CompositeC1Contrib.NewRelic;
+
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+[assembly: PreApplicationStartMethod(typeof(AppStart), "Start")]
+
+namespace CompositeC1Contrib.NewRelic
+{
+    public static class AppStart
+    {
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(NewRelicHttpModule));
+        }
+    }
+}
--- NewRelic/Properties/AssemblyInfo.cs
+++ NewRelic/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CompositeC1Contrib.NewRelic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Acto A/S")]
+[assembly: AssemblyCompany("BurningIce")]
 [assembly: AssemblyProduct("CompositeC1Contrib.NewRelic")]
-[assembly: AssemblyCopyright("Copyright © Acto A/S 2014")]
+[assembly: AssemblyCopyright("Copyright © BurnngIce A/S 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
