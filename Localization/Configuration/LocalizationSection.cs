fe7ad8bc35ae50875d739de499d2fb2781610140
Jimmy <jn@cphcloud.com>
2017-07-27 13:11:35 +0200

    Adds ability to configure whether resource keys should case sensitive in Localization

---
+++ Localization/Configuration/LocalizationSection.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+
+namespace CompositeC1Contrib.Localization.Configuration
+{
+    public class LocalizationSection : ConfigurationSection
+    {
+        private const string ConfigPath = "compositeC1Contrib/localization";
+
+        public static LocalizationSection GetSection()
+        {
+            return ConfigurationManager.GetSection(ConfigPath) as LocalizationSection;
+        }
+
+        [ConfigurationProperty("ignoreCase", IsRequired = false, DefaultValue = false)]
+        public bool IgnoreCase
+        {
+            get { return (bool)this["ignoreCase"]; }
+            set { this["ignoreCase"] = value; }
+        }
+    }
+}
