3d92e1a62f68e11a4cc828bbc916ba8d6d2e3fb1
Pauli Østerø <pauli@punktum.gl>
2019-11-19 14:03:13 -0300

    [ScheduledTasks] Allowing C1CMSContainerJobActivator to implicitly construct instances which haven't been added to the ServiceCollection

--- ScheduledTasks/C1CMSContainerJobActivator.cs
+++ ScheduledTasks/C1CMSContainerJobActivator.cs
@@ -4,13 +4,15 @@ using Composite.Core;
 
 using Hangfire;
 
+using Microsoft.Extensions.DependencyInjection;
+
 namespace CompositeC1Contrib.ScheduledTasks
 {
     public class C1CMSContainerJobActivator : JobActivator
     {
         public override object ActivateJob(Type type)
         {
-            return ServiceLocator.GetService(type);
+            return ServiceLocator.GetService(type) ?? ActivatorUtilities.CreateInstance(ServiceLocator.ServiceProvider, type);
         }
     }
 }
