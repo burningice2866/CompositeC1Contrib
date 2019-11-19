dcc8980f3e40d42e08a8d4075fee0c328e16d568
Pauli Østerø <pauli@punktum.gl>
2019-11-19 15:36:35 -0300

    [Core] Simple AddOrUpdate extension method for DataConnection

---
+++ Core/Data/DataConnectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Composite.Data;
+
+namespace CompositeC1Contrib.Data
+{
+    public static class DataConnectionExtensions
+    {
+        public static T AddOrUpdate<T>(this DataConnection data, T itm) where T : class, IData
+        {
+            if (itm.DataSourceId.ExistsInStore)
+            {
+                data.Update(itm);
+            }
+            else
+            {
+                itm = data.Add(itm);
+            }
+
+            return itm;
+        }
+    }
+}
