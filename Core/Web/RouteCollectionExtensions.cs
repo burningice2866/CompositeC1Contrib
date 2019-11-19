a674f87fca1810dec80c26afc9613e84e9cd6657
Pauli Østerø <pauli@punktum.gl>
2019-11-19 15:35:28 -0300

    [Core] Adding a ServiceBased route handler
    Updated C1 to 6.2 to access ServiceLocator/DependencyInjection features

--- Core/Web/RouteCollectionExtensions.cs
+++ Core/Web/RouteCollectionExtensions.cs
@@ -9,5 +9,30 @@ namespace CompositeC1Contrib.Web
         {
             routes.Add(new Route(route, new GenericRouteHandler<T>()));
         }
+
+        public static void AddGenericHandler<T>(this RouteCollection routes, string route, RouteValueDictionary constraints) where T : IHttpHandler, new()
+        {
+            var r = new Route(route, new GenericRouteHandler<T>())
+            {
+                Constraints = constraints
+            };
+
+            routes.Add(r);
+        }
+
+        public static void AddServiceHandler<T>(this RouteCollection routes, string route) where T : class, IHttpHandler
+        {
+            routes.Add(new Route(route, new ServiceBasedRouteHandler<T>()));
+        }
+
+        public static void AddServiceHandler<T>(this RouteCollection routes, string route, RouteValueDictionary constraints) where T : class, IHttpHandler
+        {
+            var r = new Route(route, new ServiceBasedRouteHandler<T>())
+            {
+                Constraints = constraints
+            };
+
+            routes.Add(r);
+        }
     }
 }
---
+++ Core/Web/ServiceBasedRouteHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using System.Web.Routing;
+
+using Composite.Core;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CompositeC1Contrib.Web
+{
+    public class ServiceBasedRouteHandler<T> : IRouteHandler where T : class, IHttpHandler
+    {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return ServiceLocator.GetService<T>() ?? ActivatorUtilities.CreateInstance<T>(ServiceLocator.ServiceProvider);
+        }
+    }
+}
